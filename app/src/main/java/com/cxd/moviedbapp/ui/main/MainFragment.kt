9e918cf8d75af9b1c09cc540f4bafa1eb3dc736d
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-11-07 18:03:11 +0800

    Initial commit

---
+++ app/src/main/java/com/cxd/moviedbapp/ui/main/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.cxd.moviedbapp.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cxd.moviedbapp.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+
+    }
+
+}
