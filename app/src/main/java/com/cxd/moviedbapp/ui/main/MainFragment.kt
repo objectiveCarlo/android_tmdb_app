a4941f51b5cf503a8c06e7b2b8f49329f21409fa
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-11-08 21:33:45 +0800

    Add basic views

--- app/src/main/java/com/cxd/moviedbapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/cxd/moviedbapp/ui/main/MainFragment.kt
@@ -6,28 +6,117 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cxd.moviedbapp.R
+import com.cxd.moviedbapp.databinding.MainFragmentBinding
+import com.cxd.moviedbapp.features.popular.view.PopularMoviesAdapter
+import com.cxd.moviedbapp.features.popular.viewmodels.PopularMoviesViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
-
+    private lateinit var binding: MainFragmentBinding
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: PopularMoviesViewModel by activityViewModels<PopularMoviesViewModel>()
+    private val adapter = PopularMoviesAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @ExperimentalPagingApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        setUpAdapter()
+        startSearchJob()
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            adapter.refresh()
+        }
+    }
 
+
+    private var searchJob: Job? = null
+
+
+    @ExperimentalPagingApi
+    private fun startSearchJob() {
+
+        searchJob?.cancel()
+        searchJob = lifecycleScope.launch {
+            viewModel.getMovies()
+                .collectLatest {
+                    adapter.submitData(it)
+                }
+        }
+    }
+
+//    private fun snackBarClickedPlayer(name: String) {
+//        val parentLayout = findViewById<View>(android.R.id.content)
+//        Snackbar.make(parentLayout, name, Snackbar.LENGTH_LONG)
+//            .show()
+//    }
+
+    private fun setUpAdapter() {
+
+        binding.allProductRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+//        binding.allProductRecyclerView.adapter = adapter.withLoadStateFooter(
+//            footer = PlayersLoadingStateAdapter { retry() }
+//        )
+        binding.allProductRecyclerView.adapter = adapter
+        adapter.addLoadStateListener { loadState ->
+
+            if (loadState.mediator?.refresh is LoadState.Loading) {
+
+                if (adapter.snapshot().isEmpty()) {
+                    binding.progress.isVisible = true
+                }
+                binding.errorTxt.isVisible = false
+
+            } else {
+                binding.progress.isVisible = false
+                binding.swipeRefreshLayout.isRefreshing = false
+
+                val error = when {
+                    loadState.mediator?.prepend is LoadState.Error -> loadState.mediator?.prepend as LoadState.Error
+                    loadState.mediator?.append is LoadState.Error -> loadState.mediator?.append as LoadState.Error
+                    loadState.mediator?.refresh is LoadState.Error -> loadState.mediator?.refresh as LoadState.Error
+
+                    else -> null
+                }
+                error?.let {
+                    if (adapter.snapshot().isEmpty()) {
+                        binding.errorTxt.isVisible = true
+                        binding.errorTxt.text = it.error.localizedMessage
+                    }
+
+                }
+
+            }
+        }
+
+    }
+
+    private fun retry() {
+        adapter.retry()
     }
 
 }
