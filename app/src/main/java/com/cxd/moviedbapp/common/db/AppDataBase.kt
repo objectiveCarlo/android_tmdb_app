bc5ff4b59cbc649a09cbc3674503065a8b77b4f5
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2021-11-09 21:31:36 +0800

    Implement favorite local cache

--- app/src/main/java/com/cxd/moviedbapp/common/db/AppDataBase.kt
+++ app/src/main/java/com/cxd/moviedbapp/common/db/AppDataBase.kt
@@ -6,17 +6,20 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.cxd.moviedbapp.common.dao.RemoteKeysDao
 import com.cxd.moviedbapp.common.models.paging.RemoteKeysEntity
+import com.cxd.moviedbapp.features.favorites.datasource.FavoriteMovieDao
+import com.cxd.moviedbapp.features.favorites.datasource.FavoriteMovieEntity
 import com.cxd.moviedbapp.features.popular.datasource.local.PopularMovieEntity
 import com.cxd.moviedbapp.features.popular.datasource.local.PopularMoviesDao
 
 @Database(
-    entities = [PopularMovieEntity::class, RemoteKeysEntity::class],
+    entities = [PopularMovieEntity::class, RemoteKeysEntity::class, FavoriteMovieEntity::class],
     version = 1, exportSchema = false
 )
 abstract class AppDataBase : RoomDatabase() {
 
     abstract val popularMovieDao: PopularMoviesDao
     abstract val remoteKeysEntityDao: RemoteKeysDao
+    abstract val favoriteMovieDao: FavoriteMovieDao
 
     companion object {
         @Volatile
--- app/src/main/java/com/cxd/moviedbapp/ui/main/MovieDetailFragment.kt
+++ app/src/main/java/com/cxd/moviedbapp/ui/main/MovieDetailFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import coil.load
 import coil.transform.RoundedCornersTransformation
 import com.cxd.moviedbapp.databinding.MovieDetailFragmentBinding
+import com.cxd.moviedbapp.features.favorites.viewmodels.FavoriteMoviesViewModel
+import com.cxd.moviedbapp.features.popular.viewmodels.PopularMoviesViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MovieDetailFragment: BottomSheetDialogFragment() {
     private val args: MovieDetailFragmentArgs by navArgs()
 
+    private val viewModel: FavoriteMoviesViewModel by activityViewModels()
     private var _binding: MovieDetailFragmentBinding? = null
     private val binding: MovieDetailFragmentBinding
         get() = _binding!!
@@ -30,6 +35,16 @@ class MovieDetailFragment: BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         drawView()
         setClickListeners()
+        subscriptions()
+    }
+    private fun onLoadCheckIfFavorite(id: Int) {
+        viewModel.syncFavorite(id)
+    }
+    private fun subscriptions() {
+        viewModel.isFavorite.observe(this,  {
+            binding.favorite.isEnabled = true
+            binding.favorite.isChecked = it
+        })
     }
 
     private fun drawView() {
@@ -41,12 +56,13 @@ class MovieDetailFragment: BottomSheetDialogFragment() {
                 crossfade(durationMillis = 1500)
                 transformations(RoundedCornersTransformation(12.5f))
             }
+            onLoadCheckIfFavorite(id)
         }
     }
 
     private fun setClickListeners() {
         binding.favorite.setOnClickListener {
-
+            viewModel.favorite(args.movie.id, binding.favorite.isChecked)
         }
     }
 
